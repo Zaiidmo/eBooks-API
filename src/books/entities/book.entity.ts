8899b371f28a718abd707f86f6eeed5a8f35ce07
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-12-23 10:15:59 +0100

    BOOKS: Create the books entity

--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -1,1 +1,19 @@
-export class Book {}
+export class Book {
+    book_id: string;
+    title: string;
+    author: string;
+    category: string;
+    quantity: number;
+    cover: string;
+    description: string;
+    price: number;
+    borrowedBy?: BorrowInfo[];
+    createdAu: string;
+    updatedAu: string;
+}
+
+export class BorrowInfo {
+    user_id: string;
+    borrow_date: string;
+    dueDate: string;
+}
