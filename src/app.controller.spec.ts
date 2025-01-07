99d2643e91f688c397bee9389811ff0a6c1cd1f7
Zaiidmo <Zaiidmoumnii@gmail.com>
2025-01-07 09:53:27 +0100

    Update

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe("v.1.0.0");
+      expect(appController.getHello()).toBe("Debriefing");
     });
   });
 });
